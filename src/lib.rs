f9eced3685de6618acf173e45f6c09d4470b5f55
b1tg <b1tg@users.noreply.github.com>
2021-08-26 10:13:07 +0800

    Add struct Weekday and parse it

--- src/lib.rs
+++ src/lib.rs
@@ -35,11 +35,44 @@ mod tests {
         // let dt = DateTime::parse_from_str("1983 Apr 13 12:09:14.274 +0000", "%Y %b %d %H:%M:%S%.3f %z");
         // let dt = DateTime::parse_from_str(case1, "%Y年%m月%d日星期 %H:%M:%S%.3f %z");
 
-        let re =
-            Regex::new(r"(\d{4})年(\d\d?)月(\d\d?)日星期(.*) (.*)(\d{2}):(\d{2}):(\d{2})").unwrap();
+        // let re =
+        //     Regex::new(r"(\d{4})年(\d\d?)月(\d\d?)日星期(.*) (.*)(\d{2}):(\d{2}):(\d{2})").unwrap();
 
-        let cap = re.captures(case1).unwrap();
+        // let cap = re.captures(case1).unwrap();
+        // dbg!(&cap);
+        let cap = parse_date(case1);
         dbg!(&cap);
+
+        Weekday::from_str("二");
+        Weekday::from_str("xx");
+        Weekday::from_str("x");
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Weekday {
+    Mon,
+    Tue,
+    Wed,
+    Thu,
+    Fri,
+    Sat,
+    Sun,
+}
+
+impl Weekday {
+    pub fn from_str(wd: &str) -> Option<Self> {
+        let weekday = match wd {
+            "一" => Weekday::Mon,
+            "二" => Weekday::Tue,
+            "三" => Weekday::Wed,
+            "四" => Weekday::Thu,
+            "五" => Weekday::Fri,
+            "六" => Weekday::Sat,
+            "日" => Weekday::Sun,
+            _ => return None,
+        };
+        Some(weekday)
     }
 }
 
@@ -55,7 +88,35 @@ pub struct Datetime {
     second: u8,
 }
 
-pub fn parse_date(input: &str) {}
+pub fn parse_date(input: &str) -> Result<Datetime, ()> {
+    let re =
+        Regex::new(r"(\d{4})年(\d\d?)月(\d\d?)日星期(.*) (.*)(\d{2}):(\d{2}):(\d{2})").unwrap();
+
+    if let Some(caps) = re.captures(input) {
+        let internal_parse = |idx| -> usize {
+            caps.get(idx)
+                .map(|x| x.as_str())
+                .unwrap_or("")
+                .parse()
+                .unwrap_or_default()
+        };
+        let internal_parse_str =
+            |idx| -> String { caps.get(idx).map(|x| x.as_str()).unwrap_or("").to_string() };
+        assert_eq!(caps.len(), 9);
+        let mut datetime = Datetime::default();
+        // datetime.year = caps.get(1).map(|x|x.as_str()).unwrap_or("").parse().unwrap_or_default();
+        datetime.year = internal_parse(1) as _;
+        datetime.month = internal_parse(2) as _;
+        datetime.day = internal_parse(3) as _;
+        datetime.weekday = internal_parse_str(4) as _;
+        datetime.am_or_pm = internal_parse_str(5) as _;
+        datetime.hour = internal_parse(6) as _;
+        datetime.minute = internal_parse(7) as _;
+        datetime.second = internal_parse(8) as _;
+        return Ok(datetime);
+    }
+    return Err(());
+}
 
 pub fn parse_desc(input: &str) -> Result<Desc, ()> {
     let re = Regex::new(r"#(\d*)(-(\d*))?.*(20\d\d年.*)$").unwrap();
